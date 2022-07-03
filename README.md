e4d9bef80016535f4074a9b441905b4f423e3485
Just-a-NoobieDev <carljames.roxas.m@gmail.com>
2022-07-03 17:46:46 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to my Personal Portfolio Website
+
+This project is built to showcase my personal projects all throughout my journey to become a professional web developer
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>THIS PORTFOLIO IS UNDER CONSTRUCTION.</h1>
+    </div>
+  );
+}
+
+export default App;
