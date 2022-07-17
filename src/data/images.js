c55ea6a3fd064f28fbfe48d750832eff36e52fc4
Just-a-NoobieDev <carljames.roxas.m@gmail.com>
2022-07-17 17:16:58 +0800

    Update data.json

--- src/data/images.js
+++ src/data/images.js
@@ -1,6 +1,7 @@
-import image1 from "../images/test_image.jpg";
+import image1 from "../images/react-portfolio.png";
 import image2 from "../images/test.png";
+import image3 from "../images/test_image.jpg";
 
-const images = [image1, image2, image1, image2, image1, image2];
+const images = [image1, image2, image3, image2, image3, image2];
 
 export default images;
