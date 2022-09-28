280753ffe3f28a98b168d8b7074f2ce1b49c2a0a
Just-a-NoobieDev <carljames.roxas.m@gmail.com>
2022-09-28 22:39:39 +0800

    Adding another projects

--- src/data/images.js
+++ src/data/images.js
@@ -1,7 +1,9 @@
 import cryptowhales from "../images/meta-img.png";
 import erms from "../images/ERMS.png";
 import portfolio from "../images/react-portfolio.png";
+import netflix from "../images/netflix-prev.png";
+import covid19 from "../images/website-preview-covid-19today.png";
 
-const images = [portfolio, cryptowhales, erms];
+const images = [portfolio, cryptowhales, erms, netflix, covid19];
 
 export default images;
