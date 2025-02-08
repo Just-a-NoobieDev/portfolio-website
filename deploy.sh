74cf3ed057af330a765ade01877cd3df545a5ae2
Carl James Roxas <108602920+Just-a-NoobieDev@users.noreply.github.com>
2025-02-08 16:21:10 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+set -e
+
+echo "Starting deployment process..."
+
+DEPLOY_DIR="/home/carl/projects/portfolio-website"
+
+cd $DEPLOY_DIR
+
+echo "Pulling latest changes..."
+git pull origin main
+
+echo "Install packages"
+yarn install
+
 echo "Running Docker Compose"
 docker compose up -d
 
